0c91eb422a38056cd9d6117e77adf94aab370fb5
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-26 22:40:02 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -28,7 +28,7 @@ class MusicLibraryController
         when "list genres"
           list_genres
         when "list artist"
-          list_artist
+          list_songs_by_artist
         when
           
         else
